d83813865e64864cc7e2b5ef61e921829c159ff5
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-10-24 08:59:11 +0500

    refactor admin layout: remove AddProject and Dashboard components, update Sidebar styles for improved layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,14 +1,15 @@
 import AdminNavabar from '@/components/admin/AdminNavabar'
 import Sidebar from '@/components/admin/Sidebar'
 import React from 'react'
-import Dashboard from './page'
+import Dashboard from './Dashboard'
 
 function Layout() {
   return (
     <div>
       <AdminNavabar/>
-      <div className='mt-20 flex flex-row w-full'>
+      <div className='mt-16 flex flex-row w-full'>
       <Sidebar/>
+
       <Dashboard/>
       </div>
     </div>
--- components/admin/Sidebar.tsx
+++ components/admin/Sidebar.tsx
@@ -4,19 +4,19 @@ import { LayoutDashboard, FilePlus2 } from "lucide-react";
 function Sidebar() {
 
   return (
-    <aside className="md:w-64 w-20 bg-adminBG border-r border-gray-200 shadow-sm flex flex-co">
-      <nav className="flex-1 p-4 space-y-2 text-gray-700">
+    <aside className="fixed top-17 left-0 md:w-56 w-18 backdrop-blur-xl bg-black/40 h-screen border-r border-gray-200 shadow-sm flex flex-co">
+      <nav className="flex-1 p-2 md:p-4 space-y-3 text-gray-700 ">
      
             <a
               href="/admin"
-              className="flex items-center gap-3 w-full px-4 py-3 rounded-xl font-medium cursor-pointer transition-all duration-200 bg-blue-100 text-blue-600 shadow-sm hover:bg-gray-100 hover:text-gray-900"
+              className="flex items-center justify-center md:justify-start gap-3 w-full px-4 py-3 rounded-xl font-medium cursor-pointer transition-all duration-200 bg-blue-100 text-blue-600 shadow-sm hover:bg-gray-100 hover:text-gray-900"
             >
               <LayoutDashboard className="h-5 w-5" />
               <span className="hidden md:inline-block">Dashboard</span>
             </a>
             <a
               href="/admin/add-food"
-              className="flex items-center gap-3 w-full px-4 py-3 rounded-xl font-medium cursor-pointer transition-all duration-200 bg-blue-100 text-blue-600 shadow-sm hover:bg-gray-100 hover:text-gray-900"
+              className="flex items-center justify-center md:justify-start gap-3 w-full px-3 py-3 rounded-xl font-medium cursor-pointer transition-all duration-200 bg-blue-100 text-blue-600 shadow-sm hover:bg-gray-100 hover:text-gray-900"
             >
               <FilePlus2 className="h-5 w-5" />
               <span className="hidden md:inline-block">Add Food</span>
