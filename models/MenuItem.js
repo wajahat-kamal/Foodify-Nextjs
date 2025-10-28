9d3e0c5e6d13bd5b04d2dcbcab4caaa6970a4d3e
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-10-28 07:22:55 +0500

    Enhance MenuItem schema: add ingredients field as an array of strings to store ingredient names, improving data structure for menu items.

--- models/MenuItem.js
+++ models/MenuItem.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
 
-const MenuSchema = new mongoose.Schema({
-  image: { type: String, required: true },
-  name: { type: String, required: true },
-  category: { type: String, required: true },
-  price: { type: Number, required: true },
-}, { timestamps: true });
+const MenuSchema = new mongoose.Schema(
+  {
+    image: { type: String, required: true },
+    name: { type: String, required: true },
+    ingredients: { type: [String], required: true }, // <-- Array of ingredient names
+    category: { type: String, required: true },
+    price: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.models.Menu || mongoose.model("Menu", MenuSchema);
