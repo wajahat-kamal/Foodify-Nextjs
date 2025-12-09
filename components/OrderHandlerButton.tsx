0f905bd0ff584d5681816ce6c2626b26cc048c61
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-12-09 16:30:38 +0500

    Initial commit

--- components/OrderHandlerButton.tsx
+++ components/OrderHandlerButton.tsx
@@ -1,15 +1,17 @@
-'use client';
+"use client";
 import React from "react";
 import toast from "react-hot-toast";
 
-function OrderHandlerButton({name}: { name: string }) {
-
-   const orderHandler = (name: string) => {
-    toast.success(`Your order for "${name}" has been placed!`)
-   }
+function OrderHandlerButton({ name }: { name: string }) {
+  const orderHandler = (name: string) => {
+    toast.success(`Your order for "${name}" has been placed!`);
+  };
 
   return (
-    <button onClick={() => orderHandler(name)} className="mt-4 bg-[#F0B100] hover:bg-[#d99c00] text-black font-semibold text-lg px-6 py-3 rounded-xl shadow-lg transition-all duration-300 w-fit">
+    <button
+      onClick={() => orderHandler(name)}
+      className="mt-4 bg-[#F0B100] hover:bg-[#d99c00] text-black font-semibold text-lg px-6 py-3 rounded-xl shadow-lg transition-all duration-300 w-fit"
+    >
       Order Now
     </button>
   );
